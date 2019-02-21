28c78ceaaaddbe7aae5da642bf32856692a2b6a2
krutovsky-danya <krutovsky.danya@gmail.com>
2019-02-21 22:11:36 +0500

    Не могу в кнопки, помогите

--- Project.py
+++ Project.py
@@ -6,7 +6,7 @@ Created on Sat Feb 16 19:27:10 2019
 """
 
 from PyQt5.QtCore import QRect
-from PyQt5.QtGui import QPixmap
+#from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (QApplication,
                              QDialog,
                              QGridLayout,
@@ -43,19 +43,19 @@ class Cloud_Folder(QDialog):
         
         self.setWindowTitle("Cloud Folder")
         #self.setWindowIcon()
-        self.setGeometry(100, 100, 200, 300)
+        self.setGeometry(100, 100, 900, 450)
     
     def createTree(self):
         self.Tree = QVBoxLayout()
         
-        Danya = QPushButton("Danya")
-        Danya.setFlat(True)
+        self.root = QPushButton(self.home)
+        self.root.setFlat(True)
         
-        self.Tree.addWidget(Danya)
+        self.Tree.addWidget(self.root)
     
     def createInventory(self):
         self.Inventory = QVBoxLayout()
-        self.Inventory.setGeometry(QRect(0, 0, 100, 300))
+        self.Inventory.setGeometry(QRect(0, 0, 300, 300))
         self.changeInventory()
     
     def createTab(self):
@@ -65,28 +65,28 @@ class Cloud_Folder(QDialog):
         tab_1_layout = QVBoxLayout()
         connections = ["Server", "Nickita", "Vald", "Homework 4Tb"]
         btns = []
-        for i in connections:
-            btn = QPushButton(i)
-            btn.setFlat(True)
-            #btn.clicked.connect(self.changeInventory(i))
-            tab_1_layout.addWidget(btn)
-            btns.append(btn)
+        for i in range(len(connections)):
+            btns.append(QPushButton(connections[i]))
+            btns[i].setFlat(True)
+            btns[i].clicked.connect(lambda:self.changeInventory(_name=connections[i]))
+            tab_1_layout.addWidget(btns[i])
     
         tab_1.setLayout(tab_1_layout)
         
         tab_2 = QLabel()
-        tab_2.setPixmap(QPixmap("bebop.jpg"))
+        #tab_2.setPixmap(QPixmap("bebop.jpg"))
         
         
         self.Tab.addTab(tab_1, "&Connected")
         self.Tab.addTab(tab_2, "&Preview")
     
-    def changeInventory(self, name=None):
+    def changeInventory(self, _name=None):
         self.Inventory = QVBoxLayout()
-        if name == None:
-            name = self.home
-        for i in self.folders[name]:
+        if _name == None:
+            _name = self.home
+        for i in self.folders[_name]:
             btn = QPushButton(i)
+            print(i)
             self.Inventory.addWidget(btn)
             
         
