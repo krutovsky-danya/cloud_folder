88b15c3087094ec7e30a4f4a4afbebc25f6a4d9b
krutovsky-danya <krutovsky.danya@gmail.com>
2019-02-21 17:53:58 +0500

    Теперь что-то работает

--- Project.py
+++ Project.py
@@ -5,34 +5,45 @@ Created on Sat Feb 16 19:27:10 2019
 @author: Даня
 """
 
-from PyQt5.QtWidgets import (QApplication, QDialog, QGridLayout, QLabel,
-                             QVBoxLayout, QPushButton, QTabWidget, QWidget)
+from PyQt5.QtCore import QRect
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (QApplication,
+                             QDialog,
+                             QGridLayout,
+                             QLabel,
+                             QVBoxLayout,
+                             QPushButton,
+                             QTabWidget,
+                             QWidget)
 
-class WidgetGallery(QDialog):
+class Cloud_Folder(QDialog):
     def __init__(self, parent=None):
-        super(WidgetGallery, self).__init__(parent)
+        super(Cloud_Folder, self).__init__(parent)
         
+        self.home = "Danya"
         self.folders = {"Danya" : ["HomeWork_OfCourse", "Pictures",
                                    "Documents"],
                         "Server" : ["ReadMe.txt"],
                         "Nickita" : ["Do Not Touch"],
-                        "Vlad" : ["Do Not Touch Me!"]}
+                        "Vlad" : ["Do Not Touch Me!"],
+                        "Homework 4Tb" : ["Hmmmmm"]}
         
         self.createTree()
         self.createInventory()
         self.createTab()
         
         layout = QGridLayout()
-        layout.addLayout(self.Tree, 0, 0, 2, 1)
-        layout.addWidget(self.Inventory, 0, 1, 1, 1)
-        layout.addWidget(QLabel("Info"), 1, 1)
-        layout.addWidget(self.Tab, 0, 2, 2, 1)
+        layout.addWidget(QLabel("Path[S:Dnaya//"), 0, 0, 1, 3)
+        layout.addLayout(self.Tree, 1, 0, 2, 1)
+        layout.addLayout(self.Inventory, 1, 1)
+        layout.addWidget(QLabel("Info"), 2, 1)
+        layout.addWidget(self.Tab, 1, 2, 2, 1)
         
         self.setLayout(layout)
         
         self.setWindowTitle("Cloud Folder")
         #self.setWindowIcon()
-        self.setGeometry(100, 100, 500, 300)
+        self.setGeometry(100, 100, 200, 300)
     
     def createTree(self):
         self.Tree = QVBoxLayout()
@@ -43,16 +54,18 @@ class WidgetGallery(QDialog):
         self.Tree.addWidget(Danya)
     
     def createInventory(self):
-        self.changeInventory("Danya")
+        self.Inventory = QVBoxLayout()
+        self.Inventory.setGeometry(QRect(0, 0, 100, 300))
+        self.changeInventory()
     
     def createTab(self):
         self.Tab = QTabWidget()
         
         tab_1 = QWidget()
         tab_1_layout = QVBoxLayout()
-        connection = ["Server", "Nickita", "Vald", "Homework 4Tb"]
+        connections = ["Server", "Nickita", "Vald", "Homework 4Tb"]
         btns = []
-        for i in connection:
+        for i in connections:
             btn = QPushButton(i)
             btn.setFlat(True)
             #btn.clicked.connect(self.changeInventory(i))
@@ -61,13 +74,21 @@ class WidgetGallery(QDialog):
     
         tab_1.setLayout(tab_1_layout)
         
-        tab_2 = QWidget()
+        tab_2 = QLabel()
+        tab_2.setPixmap(QPixmap("bebop.jpg"))
+        
         
         self.Tab.addTab(tab_1, "&Connected")
         self.Tab.addTab(tab_2, "&Preview")
     
     def changeInventory(self, name=None):
-        self.Inventory = QLabel("Inventory")
+        self.Inventory = QVBoxLayout()
+        if name == None:
+            name = self.home
+        for i in self.folders[name]:
+            btn = QPushButton(i)
+            self.Inventory.addWidget(btn)
+            
         
 
 if __name__ == '__main__':
@@ -75,6 +96,6 @@ if __name__ == '__main__':
     import sys
 
     app = QApplication(sys.argv)
-    gallery = WidgetGallery()
+    gallery = Cloud_Folder()
     gallery.show()
     sys.exit(app.exec_()) 
