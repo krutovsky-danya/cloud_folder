19bcf1e83dc1f8c073cc9815288573bb8a7aaca8
bombanya <proshkin20020129@mail.ru>
2019-03-01 21:26:40 +0500

    Написал toolbar

--- Project.py
+++ Project.py
@@ -6,6 +6,7 @@ Created on Sat Feb 16 19:27:10 2019
 """
 
 from PyQt5.QtCore import QRect
+from PyQt5.QtGui import QIcon
 #from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (QApplication,
                              QDialog,
@@ -19,12 +20,46 @@ from PyQt5.QtWidgets import (QApplication,
                              QTreeWidgetItem,
                              QWidget,
                              QMainWindow,
-                             QFileDialog)
+                             QFileDialog,
+                             QAction)
+
+class Shell(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.main_widget = Cloud_Folder()
+        self.setCentralWidget(self.main_widget)
+
+        self.ToolBarElements = [['Download.png', 'Download from server', self.Download],
+                                ['Upload.png','Upload to server', self.Upload],
+                                ['Delete.png', 'Delete', self.Delete],
+                                ['Find_someone.png', 'Find_someone', self.Find_someone]]
+
+        self.toolbar = self.addToolBar('Commands')
+        self.toolbar.setMovable(False)
+        for path, text, action in self.ToolBarElements:
+            newAction = QAction(QIcon('Icons//' + path), text, self)
+            newAction.triggered.connect(action)
+            self.toolbar.addAction(newAction)
+
+    def Download(self):
+        print("Download")
+
+    def Upload(self):
+        print("Upload")
+
+    def Delete(self):
+        print("Delete")
+
+    def Find_someone(self):
+        print("Find_someone")
 
 class Cloud_Folder(QWidget):
     def __init__(self, parent=None):
         super(Cloud_Folder, self).__init__(parent)
-        
+
         self.home = "Danya"
         self.folders = {"Danya" : ["HomeWork_OfCourse.", "Pictures.",
                                    "Documents."],
@@ -32,29 +67,29 @@ class Cloud_Folder(QWidget):
                         "Nickita" : ["Do Not Touch.f"],
                         "Vald" : ["Do Not Touch Me!.f"],
                         "Homework 4Tb" : ["Hmmmmm.f"]}
-        
+
         self.createTree()
-        self.createInventory()
+        #self.createInventory()
         self.createServer()
-        
+
         layout = QHBoxLayout()
         #layout.addWidget(QLabel("Path[S:Dnaya//"), 0, 0, 1, 3)
         layout.addWidget(self.Tree)
-        layout.addLayout(self.Inventory)
+        #layout.addLayout(self.Inventory)
         layout.addWidget(self.Server)
-        
+
         self.setLayout(layout)
-        
+
         self.setWindowTitle("Cloud Folder")
         #self.setWindowIcon()
         self.setGeometry(100, 100, 900, 250)
-    
+
     def createTree(self):
         self.Tree = QTreeWidget()
         self.Tree.header().setVisible(False)
         self.createLay(self.Tree, self.home)
-        
-        
+
+        '''
     def createInventory(self):
         self.Inventory = QVBoxLayout()
         self.Boxes = QHBoxLayout()
@@ -63,10 +98,10 @@ class Cloud_Folder(QWidget):
         self.Boxes.addWidget(self.UserBox)
         self.Boxes.addWidget(self.ServerBox)
         self.Inventory.addLayout(self.Boxes)
-        self.createInfo()
-        self.Inventory.addLayout(self.Info)
-        
-    
+        #self.createInfo()
+        #self.Inventory.addLayout(self.Info)
+        '''
+        '''
     def createInfo(self):
         self.Info = QHBoxLayout()
         self.Info.addWidget(QLabel("Info:"))
@@ -76,26 +111,27 @@ class Cloud_Folder(QWidget):
         B = QPushButton("Find someone")
         lay.addWidget(B)
         self.Info.addLayout(lay)
-    
+        '''
+
     def createServer(self):
         self.Server = QTabWidget()
-        
+
         tab_1 = QTreeWidget()
         tab_1.header().setVisible(False)
         tab_1_layout = QVBoxLayout()
         connections = ["Server", "Nickita", "Vald", "Homework 4Tb"]
         for i in connections:
             self.createLay(tab_1, i)
-    
+
         tab_1.setLayout(tab_1_layout)
-        
+
         tab_2 = QLabel()
         #tab_2.setPixmap(QPixmap("bebop.jpg"))
-        
-        
+
+
         self.Server.addTab(tab_1, "&Connected")
         self.Server.addTab(tab_2, "&Preview")
-    
+
     def changeInventory(self, _name=None):
         self.Inventory = QVBoxLayout()
         if _name == None:
@@ -104,21 +140,20 @@ class Cloud_Folder(QWidget):
             btn = QPushButton(i)
             print(i)
             self.Inventory.addWidget(btn)
-            
+
     def createLay(self, home, lay):
         a = QTreeWidgetItem(home, [lay])
         if lay.count('.') == 0:
             for i in self.folders[lay]:
                 self.createLay(a, i)
 
-        
+
 
 if __name__ == '__main__':
 
     import sys
 
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setCentralWidget(Cloud_Folder())
+    win = Shell()
     win.show()
-    sys.exit(app.exec_()) 
+    sys.exit(app.exec_())
