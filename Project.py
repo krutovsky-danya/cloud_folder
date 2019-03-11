6cc5f041eb38be43570d384d162ad581cb464526
krutovsky-danya <krutovsky.danya@gmail.com>
2019-03-11 19:20:04 +0500

    Поможешь с Юри?

--- Project.py
+++ Project.py
@@ -6,9 +6,10 @@ Created on Sat Feb 16 19:27:10 2019
 import csv
 #from PyQt5.QtCore import (QRect, Qt)
 from PyQt5.QtGui import (QPixmap,
-                         QIcon)
+                         QIcon,
+                         QMovie)
 from PyQt5.QtWidgets import (QApplication,
-                             #QDialog,
+                             QDialog,
                              #QGridLayout,
                              QLabel,
                              QHBoxLayout,
@@ -362,4 +363,31 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = Shell()
     win.show()
+    
+    loading = QDialog()
+    firstlay = QHBoxLayout()
+    first = QLabel()
+    dance = QMovie('Icons//loading.gif')
+    first.setMovie(dance)
+    dance.start()
+    firstlay.addWidget(first)
+    loading.setLayout(firstlay)
+    loading.setWindowTitle('Loading')
+    loading.setWindowIconText(nanachi)
+    loading.setWindowIcon(QIcon(QPixmap('Icons//Tsu.jpg')))
+    loading.show()
+    
+    topLoading = QDialog()
+    secondlay = QHBoxLayout()
+    second = QLabel()
+    parade = QMovie('Icons//topLoading.gif')
+    second.setMovie(parade)
+    parade.start()
+    secondlay.addWidget(second)
+    topLoading.setLayout(secondlay)
+    topLoading.setWindowTitle('TopLoading')
+    topLoading.setWindowIconText(nanachi)
+    topLoading.setWindowIcon(QIcon(QPixmap('Icons//Uri.png')))
+    topLoading.show()
+        
 sys.exit(app.exec_())
