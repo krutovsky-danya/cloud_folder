7924cdffed8b31dbe181002c0670ab2fa3d6e3be
krutovsky-danya <krutovsky.danya@gmail.com>
2019-03-22 18:15:55 +0500

    Теперь работает, но есть баг

--- Project.py
+++ Project.py
@@ -11,8 +11,6 @@ from Shell import Shell
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     win = Shell()
-    win.main_widget.ListOfUserFolders[self.main_widget.user_id].setSelected()
-    win.main_widget.ListOfServerFolders[0].setSelected()
     win.show()
 
     sys.exit(app.exec_())
